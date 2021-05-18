26242c5fbd6eb5e7eb7a759acd8ca9c5dbfa79f4
Rita <ryta.borgez@gmail.com>
2021-05-18 23:26:10 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/EDAF_eFolioB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDAF_eFolioB.dir/main.cpp.o"
+  "EDAF_eFolioB"
+  "EDAF_eFolioB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EDAF_eFolioB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
